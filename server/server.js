6366f55c6d1b03ce03f6780b4d44e6cdea5eec15
Noah Fernandes <noahdecoco@gmail.com>
2017-06-06 09:21:31 +0200

    First commit

---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
+app.get('/todos', (req, res) => {
+
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+
+});
+
+app.get('/todos/:id', (req, res) => {
+
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)  ) return res.status(404).send();
+
+  Todo.findById(id).then((todo) => {
+
+    if(!todo) return res.status(404).send();
+
+    res.send({todo});
+
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
+module.exports = {app};
---
+++ server/server.test.js
@@ -0,0 +1,79 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./server');
+const {Todo} = require('./models/todo');
+
+const todos = [{text: 'Todo 1'},{text: 'Todo 2'}];
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
+
+describe('POST /todos', () => {
+
+  it('should create a new todo', (done) => {
+
+    var text = 'todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if(err) return console.log(err);
+
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+  it('should not create a new todo with invalid data', (done) => {
+
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if(err) return console.log(err);
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(2);
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+});
+
+
+describe('GET /todos', () => {
+
+  it('should get a list of todos', (done) => {
+
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .end((err, res) => {
+      if(err) return console.log(err);
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(2);
+        expect(todos).toBeA('array');
+        done();
+      }).catch((e) => done(e));
+    });
+
+  });
+
+});
