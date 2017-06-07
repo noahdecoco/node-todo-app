44dae78f6667ae4b56aa3678a3c43d5df86c5c47
Noah Fernandes <noah@Noahs-iMac.local>
2017-06-07 18:05:13 +0200

    Added environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
+switch(process.env.NODE_ENV) {
+  case 'test':
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp-test';
+    break;
+  case 'dev':
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp-dev';
+    break;
+  case 'prod':
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    break;
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +23,6 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
 let app = express();
-const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -118,8 +132,8 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
 
 module.exports = {app};
