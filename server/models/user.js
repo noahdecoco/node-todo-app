e4217a735da9d62a02cdef5d4964f90131e87150
Noah Fernandes <noahdecoco@gmail.com>
2017-06-10 13:35:13 +0200

    added crypting for passwords

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -30,6 +31,23 @@ var UserSchema = new mongoose.Schema({
           required: true
         }
     }]
+});
+
+UserSchema.pre('save', function(next) {
+
+  var user = this;
+
+  if(user.isModified('password')) {
+      bcryptjs.genSalt(10, (err, salt) => {
+        bcryptjs.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
+        });
+      });
+  } else {
+    next();
+  }
+
 });
 
 UserSchema.methods.toJSON = function() {
